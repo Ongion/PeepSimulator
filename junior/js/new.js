2532b52c25825867b732562c2592cd1822693e03
Kyle Bippus <kbipp49@gmail.com>
2013-02-13 23:05:16 -0500

    Work on R Chart

--- junior/js/new.js
+++ junior/js/new.js
@@ -59,10 +59,14 @@ var sqtotals = {A : 0, B : 0, C : 0};
 var sqtotal = sqtotals['A'];
 // Google Charts Vars
 var chart;
+var rchart;
 var spreadsheet;
 var currentData;
+var currentRData;
 var datasets;
+var datasetsR;
 var emptyData;
+var emptyRData;
 var chartDataAdapter;
 var spreadsheetDataAdapter;
 var spreadsheetOptions = {
@@ -83,36 +87,58 @@ var chartOptions = {
 // Start Everything
 function initializeData() {
 	var cols = new Array('Peep Number', 'Fluffiness', 'Mean', 'UCL', 'LCL');
+	var rcols = new Array('Peep Number', 'Fluffiness', 'Range', 'UCL', 'LCL');
 	datasets = {A : new google.visualization.DataTable(), B : new google.visualization.DataTable(), C : new google.visualization.DataTable()};
-	emptyData = new google.visualization.DataTable()
+	datasetsR = {A : new google.visualization.DataTable(), B : new google.visualization.DataTable(), C : new google.visualization.DataTable()};
+	emptyData = new google.visualization.DataTable();
+	emptyRData = new google.visualization.DataTable();
 	for (var i = 0; i < cols.length; i++) {
 		datasets['A'].addColumn('number', cols[i]);
 		datasets['B'].addColumn('number', cols[i]);
 		datasets['C'].addColumn('number', cols[i]);
 		emptyData.addColumn('number', cols[i]);
 	}
+	for (i = 0; i < rcols.length; i++) {
+		datasetsR['A'].addColumn('number', cols[i]);
+		datasetsR['B'].addColumn('number', cols[i]);
+		datasetsR['C'].addColumn('number', cols[i]);
+		emptyRData.addColumn('number', cols[i]);
+	}
 	changePlan('A');
 	chart = new google.visualization.LineChart(document.getElementById('chart'));
+	rchart = new google.visualization.LineChart(document.getElementById('rchart'));
 	spreadsheet = new google.visualization.Table(document.getElementById('spreadsheet'));
 	updateCharts();
 	
 	google.visualization.events.addListener(chart, 'select', function() {spreadsheet.setSelection([{row: chart.getSelection()[0].row}]);});
+	google.visualization.events.addListener(rchart, 'select', function() {spreadsheet.setSelection([{row: rcharts.getSelection()[0].row}]);});
 	google.visualization.events.addListener(spreadsheet, 'select', function() {chart.setSelection(spreadsheet.getSelection());});
 	selectMachine();
 }
 function updateCharts() {
-	chartData = new google.visualization.DataView(currentData);
+	var chartData = new google.visualization.DataView(currentData);
 	chartData.setColumns([0, 1, 2, 3, 4]);
-	drawChart();
-	spreadsheetData = new google.visualization.DataView(currentData);
+
+	var rchartData = new google.visualizatoin.DataView(currentRData);
+	rchartData.setColumns([0, 1, 2, 3, 4]);
+	drawRChart();
+
+	var spreadsheetData = new google.visualization.DataView(currentData);
 	spreadsheetData.setColumns([0, 1]);
 	drawSpreadsheet();
 }
 // Shortcuts
 function drawChart() {
-	if (currentData.getNumberOfRows() >= 25 && chart) chart.draw(chartData, chartOptions);
-	else if (chart) chart.draw(emptyData, chartOptions);
+	if (currentData.getNumberOfRows() >= 25 && chart){
+		chart.draw(chartData, chartOptions);
+		rchart.draw(rchartData, chartOptions);
+	}
+	else if (chart && rchart) {
+		chart.draw(emptyData, chartOptions);
+		rchart.draw(emptyRData, chartOptions);
+	}
 }
+
 function drawSpreadsheet() {if (spreadsheet) spreadsheet.draw(spreadsheetData, spreadsheetOptions);}
 // Data Manipulators
 function addRow(num, machine) {
@@ -124,6 +150,7 @@ function addRow(num, machine) {
 		currentData.controlLimit = 3 * Math.sqrt((sqtotal - total * currentData.mean) / count);
 	}
 	currentData.addRow([count, num, currentData.mean, currentData.mean + currentData.controlLimit, currentData.mean - currentData.controlLimit]);
+	currentRData.addRow([count, num, currentRData.
 	setMean(count, currentData.mean, currentData.controlLimit);
 	buffer.length = 0;
 	drawSpreadsheet();
@@ -196,8 +223,19 @@ function planC() {
 }
 // Math Helpers
 function uniformRandom(min, max) {return min + Math.random() * (max - min);}
+
 function mean(array) {
 	var total = 0;
 	$.each(array, function() {total += this;});
 	return Math.round(total / array.length);
+}
+
+function range(array) {
+	var max = array[0];
+	var min = array[0];
+
+	for (i = 1; i < array.length; i++) {
+		if (array[i] > max) max = array[i];
+		if (array[i] < min) min = array[i];
+	}
 }
